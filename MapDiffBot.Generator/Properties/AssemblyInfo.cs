61b35d78e40b24e59d2734b3ad50c91a84d8deea
Cyberboss <Cyberboss@users.noreply.github.com>
2018-01-12 12:28:27 -0500

    Add skeleton generator project

---
+++ MapDiffBot.Generator/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MapDiffBot diff generator module")]
+[assembly: AssemblyDescription("Generates the difference between two .dmm files")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ff7e833c-d4aa-48fd-9d9c-cedc4481ceef")]
