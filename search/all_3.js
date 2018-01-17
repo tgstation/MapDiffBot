c3795d4d6b5c39786b3de29b865ba90f9c8559aa
Cyberboss <Cyberboss@users.noreply.github.com>
2018-01-17 13:05:50 -0800

    Deploy code docs to GitHub Pages for Appveyor build 19
    Commit: 8b0bba6ffc585871048bd5562ccc1fe6dc7ef056

--- search/all_3.js
+++ search/all_3.js
@@ -12,5 +12,7 @@ var searchData=
   ['diffregion',['DiffRegion',['../class_map_diff_bot_1_1_generator_1_1_diff_region.html',1,'MapDiffBot::Generator']]],
   ['diffregion_2ecs',['DiffRegion.cs',['../_diff_region_8cs.html',1,'']]],
   ['dispose',['Dispose',['../class_map_diff_bot_1_1_generator_1_1_diff_generator.html#a62b338a3ef36d7bc6de1f4c0ddcd589f',1,'MapDiffBot.Generator.DiffGenerator.Dispose()'],['../class_map_diff_bot_1_1_web_hook_1_1_repository.html#a01e34e498100dd8c2975a8a008c32d9d',1,'MapDiffBot.WebHook.Repository.Dispose()']]],
+  ['dmm_2dtools_2ed',['dmm-tools.d',['../dmm-tools_8d.html',1,'']]],
+  ['dmmtools_2edesigner_2ecs',['DMMTools.Designer.cs',['../_d_m_m_tools_8_designer_8cs.html',1,'']]],
   ['downloadfile',['DownloadFile',['../class_map_diff_bot_1_1_web_hook_1_1_default_i_o_manager.html#a558ae30586936440a0cc469be39d2b7d',1,'MapDiffBot.WebHook.DefaultIOManager.DownloadFile()'],['../interface_map_diff_bot_1_1_web_hook_1_1_i_i_o_manager.html#a2bc532ec8bf90300b2d6022967184fae',1,'MapDiffBot.WebHook.IIOManager.DownloadFile()']]]
 ];
--- search/files_1.js
+++ search/files_1.js
@@ -3,5 +3,7 @@ var searchData=
   ['defaultiomanager_2ecs',['DefaultIOManager.cs',['../_default_i_o_manager_8cs.html',1,'']]],
   ['definitions_2ecs',['Definitions.cs',['../_definitions_8cs.html',1,'']]],
   ['diffgenerator_2ecs',['DiffGenerator.cs',['../_diff_generator_8cs.html',1,'']]],
-  ['diffregion_2ecs',['DiffRegion.cs',['../_diff_region_8cs.html',1,'']]]
+  ['diffregion_2ecs',['DiffRegion.cs',['../_diff_region_8cs.html',1,'']]],
+  ['dmm_2dtools_2ed',['dmm-tools.d',['../dmm-tools_8d.html',1,'']]],
+  ['dmmtools_2edesigner_2ecs',['DMMTools.Designer.cs',['../_d_m_m_tools_8_designer_8cs.html',1,'']]]
 ];
