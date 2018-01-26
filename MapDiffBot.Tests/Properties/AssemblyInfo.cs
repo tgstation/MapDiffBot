019f3237a088cb585fe0f4fdc684da750c7b19ea
Cyberboss <Cyberboss@users.noreply.github.com>
2018-01-26 10:36:16 -0500

    Fuck the tests, they just get in the way

--- MapDiffBot.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("MapDiffBot Test Suite")]
-[assembly: AssemblyDescription("Unit tests for the MapDiffBot")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("b2589134-1cf3-44eb-bca3-e61028c4326d")]
