7f16158f97c458d4dfdd04242d941165592fa6a3
Cyberboss <Cyberboss@users.noreply.github.com>
2018-03-10 21:01:39 -0500

    AHH

--- MapDiffBot/Core/JobSubmission.cs
+++ MapDiffBot/Core/JobSubmission.cs
@@ -12,29 +12,29 @@ namespace MapDiffBot.Core
 		/// <summary>
 		/// The text for the <see cref="Hangfire.Dashboard"/>
 		/// </summary>
-		readonly string hangfireDisplayName;
+		public string HangfireDisplayName { get; set; }
 
 		/// <summary>
 		/// The <see cref="PullRequest.Base"/> <see cref="Repository.Id"/>
 		/// </summary>
-		public long RepositoryId { get; }
+		public long RepositoryId { get; set; }
 
 		/// <summary>
 		/// The <see cref="PullRequest.Number"/>
 		/// </summary>
-		public int PullRequestNumber { get; }
+		public int PullRequestNumber { get; set; }
 
 		/// <summary>
 		/// The URL to use as a link base
 		/// </summary>
-		public string BaseUrl { get; }
+		public string BaseUrl { get; set; }
 
 		/// <summary>
 		/// Construct a <see cref="JobSubmission"/>
 		/// </summary>
 		/// <param name="pullRequest">The <see cref="PullRequest"/> being submitted</param>
 		/// <param name="baseUrl">The value of <see cref="BaseUrl"/></param>
-		/// <param name="stringLocalizer">The <see cref="IStringLocalizer"/> for formatting <see cref="hangfireDisplayName"/></param>
+		/// <param name="stringLocalizer">The <see cref="IStringLocalizer"/> for formatting <see cref="HangfireDisplayName"/></param>
 		public JobSubmission(PullRequest pullRequest, string baseUrl, IStringLocalizer<PayloadProcessor> stringLocalizer)
 		{
 			if (pullRequest == null)
@@ -42,7 +42,7 @@ namespace MapDiffBot.Core
 			if (stringLocalizer == null)
 				throw new ArgumentNullException(nameof(stringLocalizer));
 			BaseUrl = baseUrl ?? throw new ArgumentNullException(nameof(baseUrl));
-			hangfireDisplayName = stringLocalizer["Generate Map Diffs for #{0} - \"{1}\" ({2})", pullRequest.Number, pullRequest.Title, pullRequest.User.Login, pullRequest.HtmlUrl];
+			HangfireDisplayName = stringLocalizer["Generate Map Diffs for #{0} - \"{1}\" ({2})", pullRequest.Number, pullRequest.Title, pullRequest.User.Login, pullRequest.HtmlUrl];
 			RepositoryId = pullRequest.Base.Repository.Id;
 			PullRequestNumber = pullRequest.Number;
 		}
@@ -52,7 +52,7 @@ namespace MapDiffBot.Core
 		/// </summary>
 		/// <param name="issue">The <see cref="Issue"/> being submitted</param>
 		/// <param name="baseUrl">The value of <see cref="BaseUrl"/></param>
-		/// <param name="stringLocalizer">The <see cref="IStringLocalizer"/> for formatting <see cref="hangfireDisplayName"/></param>
+		/// <param name="stringLocalizer">The <see cref="IStringLocalizer"/> for formatting <see cref="HangfireDisplayName"/></param>
 		public JobSubmission(Issue issue, string baseUrl, IStringLocalizer<PayloadProcessor> stringLocalizer)
 		{
 			if (issue == null)
@@ -60,12 +60,18 @@ namespace MapDiffBot.Core
 			if (stringLocalizer == null)
 				throw new ArgumentNullException(nameof(stringLocalizer));
 			BaseUrl = baseUrl ?? throw new ArgumentNullException(nameof(baseUrl));
-			hangfireDisplayName = stringLocalizer["Generate Map Diffs for #{0} - \"{1}\" ({2})", issue.Number, issue.Title, issue.User.Login, issue.HtmlUrl];
+			HangfireDisplayName = stringLocalizer["Generate Map Diffs for #{0} - \"{1}\" ({2})", issue.Number, issue.Title, issue.User.Login, issue.HtmlUrl];
 			RepositoryId = issue.Repository.Id;
 			PullRequestNumber = issue.Number;
 		}
 
+		/// <summary>
+		/// Construct a <see cref="JobSubmission"/>. For use by <see cref="Hangfire"/>
+		/// </summary>
+		[Obsolete("For use by hangfire only", true)]
+		public JobSubmission() { }
+
 		/// <inheritdoc />
-		public override string ToString() => hangfireDisplayName;
+		public override string ToString() => HangfireDisplayName;
 	}
 }
