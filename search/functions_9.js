4243eaaee2c8c3d5bebcacbd995a8e55e5415dd7
Cyberboss <Cyberboss@users.noreply.github.com>
2018-03-23 05:39:43 -0700

    Deploy code docs to GitHub Pages for Appveyor build 28
    Commit: 9a403ef31fdcfd9e30b547789691bd7d2919d92c

--- search/functions_9.js
+++ search/functions_9.js
@@ -3,6 +3,6 @@ var searchData=
   ['loadinstallation',['LoadInstallation',['../class_map_diff_bot_1_1_core_1_1_git_hub_manager.html#a5d4c680994c44e3ff5be0ee16d2575fd',1,'MapDiffBot.Core.GitHubManager.LoadInstallation()'],['../interface_map_diff_bot_1_1_core_1_1_i_git_hub_manager.html#a6a96944ebaa49ccdf59ae403012ddf8d',1,'MapDiffBot.Core.IGitHubManager.LoadInstallation()']]],
   ['localrepository',['LocalRepository',['../class_map_diff_bot_1_1_core_1_1_local_repository.html#a56ac758e104475e0a29ffffdfaf698ca',1,'MapDiffBot::Core::LocalRepository']]],
   ['localrepositoryfactory',['LocalRepositoryFactory',['../class_map_diff_bot_1_1_core_1_1_local_repository_factory.html#acd24a5c185a853bfeee5c5cd81df30a4',1,'MapDiffBot::Core::LocalRepositoryFactory']]],
-  ['localrepositorymanager',['LocalRepositoryManager',['../class_map_diff_bot_1_1_core_1_1_local_repository_manager.html#ad52b89e4d5e3e9ba5ff4cfcc55619c14',1,'MapDiffBot::Core::LocalRepositoryManager']]],
+  ['localrepositorymanager',['LocalRepositoryManager',['../class_map_diff_bot_1_1_core_1_1_local_repository_manager.html#a70a68926b7e2dc40d9d112736eeb711e',1,'MapDiffBot::Core::LocalRepositoryManager']]],
   ['lock',['Lock',['../class_map_diff_bot_1_1_core_1_1_semaphore_slim_context.html#ab4e0a2c148590316221ecf33320763fb',1,'MapDiffBot::Core::SemaphoreSlimContext']]]
 ];
