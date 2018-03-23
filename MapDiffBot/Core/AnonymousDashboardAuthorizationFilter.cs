5cc910c98a90b37d5dee7884ee04fb64de88735f
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-03-23 04:32:28 -0400

    Hopefully fix dashboard authroization

---
+++ MapDiffBot/Core/AnonymousDashboardAuthorizationFilter.cs
@@ -0,0 +1,18 @@
+﻿using Hangfire.Annotations;
+using Hangfire.Dashboard;
+
+namespace MapDiffBot.Core
+{
+	/// <summary>
+	/// <see cref="IDashboardAuthorizationFilter"/> for anonymous access
+	/// </summary>
+	sealed class AnonymousDashboardAuthorizationFilter : IDashboardAuthorizationFilter
+	{
+		/// <summary>
+		/// Check if a given <see cref="DashboardContext"/> is authorized
+		/// </summary>
+		/// <param name="context">The <see cref="DashboardContext"/> for the operation</param>
+		/// <returns><see langword="true"/></returns>
+		public bool Authorize([NotNull] DashboardContext context) => true;
+	}
+}
