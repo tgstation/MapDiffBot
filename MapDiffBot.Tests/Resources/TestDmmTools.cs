348adeb0adafb776a7156fd28a7be8c3ed204146
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-03-24 17:18:17 -0400

    Adds tests for the type of the fuching dmm tools

---
+++ MapDiffBot.Tests/Resources/TestDmmTools.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MapDiffBot.Resources.Tests
+{
+	/// <summary>
+	/// Tests for <see cref="DmmTools"/>
+	/// </summary>
+	[TestClass]
+	public sealed class TestDmmTools
+	{
+		[TestMethod]
+		public void TestType() => Assert.AreEqual(typeof(byte[]), DmmTools.dmm_tools.GetType());
+	}
+}
