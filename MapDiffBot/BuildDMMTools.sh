089e1b9dbce6b947bb1c64b0c05581ee868306ed
Jordan Brown <Cyberboss@users.noreply.github.com>
2018-03-25 12:29:57 -0400

    Fixes docker build

--- MapDiffBot/BuildDMMTools.sh
+++ MapDiffBot/BuildDMMTools.sh
@@ -3,4 +3,7 @@
 cd ../SpacemanDMM
 cargo build -p cli --release
 cd ../MapDiffBot
+if [ ! -f ../SpacemanDMM/target/release/dmm-tools.exe ]; then
+    mv ../SpacemanDMM/target/release/dmm-tools ../SpacemanDMM/target/release/dmm-tools.exe
+fi
 cp ../SpacemanDMM/target/release/dmm-tools.exe "${1//\\//}"
